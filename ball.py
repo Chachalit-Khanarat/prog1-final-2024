f694b240b1d8d7d7dcb1a47894f6e0597fabd11e
Chachalit Khanarat <Chachalit.k@ku.th>
2024-12-06 10:43:41 +0700

    make class Ball in ball.py for the simulation

--- ball.py
+++ ball.py
@@ -1,31 +1,36 @@
 import turtle
 
-def draw_ball(color, size, x, y):
-    # draw a circle of radius equals to size at x, y coordinates and paint it with color
-    turtle.penup()
-    turtle.color(color)
-    turtle.fillcolor(color)
-    turtle.goto(x,y-size)
-    turtle.pendown()
-    turtle.begin_fill()
-    turtle.circle(size)
-    turtle.end_fill()
+class ball():
+    def __init__(self, color, size, x, y , vx, vy, mass):
+        self.color = color
+        self.size = size
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.mass = mass
 
-def move_ball(i, xpos, ypos, vx, vy, dt):
-    # update the x, y coordinates of ball i with velocity in the x (vx) and y (vy) components
-    xpos[i] += vx[i]*dt
-    ypos[i] += vy[i]*dt
+    def draw_ball(self):
+        turtle.penup()
+        turtle.color(self.color)
+        turtle.fillcolor(self.color)
+        turtle.goto(self.x,self.y-self.size)
+        turtle.pendown()
+        turtle.begin_fill()
+        turtle.circle(self.size)
+        turtle.end_fill()
+
+    def move_ball(self,dt):
+        self.x += self.vx*dt
+        self.y += self.vy*dt
 
 
-def update_ball_velocity(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius):
-    # if the ball hits the side walls, reverse the vx velocity
-    if abs(xpos[i]) > (canvas_width - ball_radius):
-        vx[i] = -vx[i]
+    def update_ball_velocity(self, canvas_width, canvas_height):
+        if abs(self.x) >= (canvas_width - self.size):
+            self.vx = -self.vx
 
-    # if the ball hits the ceiling or the floor, reverse the vy velocity
-    if abs(ypos[i]) > (canvas_height - ball_radius):
-        vy[i] = -vy[i]
-
+        if abs(self.y) >= (canvas_height - self.size):
+            self.vy = -self.vy
 
 
 
